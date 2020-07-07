9f26d56d083ae052a3662b582958ea7782c0d7a0
aschaves1976 <alessandro.chaves1976@gmail.com>
2020-07-07 18:07:45 -0300

    CRUD: POST -Apresentando Erro no comando redirect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const mongodb = require('../database/dbmongo')
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const mongodb = require('../database/dbmongo')
   mongodb.getClientes(
     function(err, docs){
       if(err){
@@ -16,4 +18,23 @@ router.get('/', function(req, res, next) {
   )  
 });
 
+/* GET - Tela de Novo Cadastro*/
+router.get('/new', function(req, res, next){
+  res.render('new', { title: 'Novo Cadastro'})
+})
+
+/* POST - Tela de Novo Cadastro*/
+router.post('/new', function(req, res, next){
+  let nome = req.body.nome
+  let idade = parseInt(req.body.idade)
+  let cidade = req.body.cidade
+
+  mongodb.insertCliente({nome, idade, cidade}, (err, res)=>{
+    if (err){
+      return console.log(`Erro no Cadastro do Novo Cliente - ${err}`)
+    }
+    res.redirect('/')
+  })
+
+})
 module.exports = router;
