0eb6113eef5ca6d2de8dc501b08235f43f43dd37
aschaves1976 <alessandro.chaves1976@gmail.com>
2020-07-07 16:59:22 -0300

    Melhorias na conexão com o Banco de Dados - Eliminar Erro collection de undefined

--- database/dbmongo.js
+++ database/dbmongo.js
@@ -1,17 +1,34 @@
 const mongoClient = require('mongodb').MongoClient
+const objectId = require('mongodb').ObjectId
 
 const url = 'mongodb://localhost:27017'
 const dbName = 'aula_mongo01'
 const customerCollection = 'clientes' // customer collection
 
-mongoClient.connect(url)
-.then(conn => global.database = conn.db(dbName),
-console.log(`Conectado na base de dados ${dbName}`)
-    )
-.catch(err => console.log(`Erro ao conectar na base de dados - ${err}`))
+function conectar (callback){
+    mongoClient.connect(url)
+    .then(conn => global.database = conn.db(dbName),
+    console.log(`Conectado na base de dados ${dbName}`),
+    callback()
+        )
+    .catch(err => console.log(`Erro ao conectar na base de dados - ${err}`))
+    
+}
 
 function getClientes(callback){
     global.database.collection(customerCollection).find({}).toArray(callback)
 }
 
-module.exports = { getClientes }
+function insertCliente(cliente, callback){
+    global.database.collection(customerCollection).insert(cliente, callback)
+}
+
+function updateCliente(cliente, callback){
+    global.database.collection(customerCollection).update({_id: objectId(cliente._id)}, cliente, callback)
+}
+
+function deleteCliente(id, callback){
+    global.database.collection(customerCollection).remove({_id: objectId(id)})
+}
+
+module.exports = { conectar, getClientes, insertCliente, updateCliente, deleteCliente }
