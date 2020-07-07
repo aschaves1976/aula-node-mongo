4ef0a17edd1a58a84aac18d2ac024613cb5adc6a
aschaves1976 <alessandro.chaves1976@gmail.com>
2020-07-07 15:45:33 -0300

    Encapsulando banco de dados e coleção dentro de variável

--- database/dbmongo.js
+++ database/dbmongo.js
@@ -1,13 +1,17 @@
 const mongoClient = require('mongodb').MongoClient
 
 const url = 'mongodb://localhost:27017'
+const dbName = 'aula_mongo01'
+const customerCollection = 'clientes' // customer collection
 
 mongoClient.connect(url)
-.then(conn => global.database = conn.db('aula_mongo01'))
-.catch(err => console.log(err))
+.then(conn => global.database = conn.db(dbName),
+console.log(`Conectado na base de dados ${dbName}`)
+    )
+.catch(err => console.log(`Erro ao conectar na base de dados - ${err}`))
 
 function getClientes(callback){
-    global.database.collection('clientes').find({}).toArray(callback)
+    global.database.collection(customerCollection).find({}).toArray(callback)
 }
 
 module.exports = { getClientes }
